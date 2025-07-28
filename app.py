0a56384a6ff6ed2bef61fe69d1ed54743f0e1ecf
Saksham <saksham.goel28@gmail.com>
2025-07-28 22:56:38 +0530

    app.py

--- app.py
+++ app.py
@@ -91,4 +91,6 @@ def search_books():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port, debug=True)
